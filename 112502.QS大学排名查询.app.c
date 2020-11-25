51dca0e8a8264de28b369d7a1f12cdaf4d0994b9
gxkjsfjlq <74294932+gxkjsfjlq@users.noreply.github.com>
2020-11-25 09:52:17 +0800

    Create 112502.QS大学排名查询.app.c

---
+++ 112502.QS大学排名查询.app.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+void main()
+{
+int x;
+printf("\n   QS大学排名查询\n");
+scanf("%d",&x);
+if(x==1)
+{
+printf("\n   麻省理工学院\n");
+}
+else if(x==2)
+{
+printf("\n   斯坦福大学\n");
+}
+else if(x==3)
+{
+printf("\n   哈佛大学\n");
+}
+else if(x==4)
+{
+printf("\n   牛津大学 \n");
+}
+else if(x==5)
+{
+printf("\n   加州理工学院\n");
+}
+else if(x==6)
+{
+printf("\n   苏黎世联邦理工大学 \n");
+}
+else if(x==7)
+{
+printf("\n  剑桥大学\n");
+}
+else if(x==8)
+{
+printf("\n   伦敦大学学院 \n");
+}
+else if(x==9)
+{
+printf("\n   帝国理工学院\n");
+}
+else if(x==10)
+{
+printf("\n  1 芝加哥大学\n");
+}
+else
+{
+printf("\n  老铁的排名太靠后了，请输入1—10呦\n");
+}
+
+}
