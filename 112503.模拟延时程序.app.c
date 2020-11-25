b17b5815c77a26ccbe5551841eac4e4743778e89
gxkjsfjlq <74294932+gxkjsfjlq@users.noreply.github.com>
2020-11-25 16:51:35 +0800

    Create 112503.模拟延时程序.app.c

---
+++ 112503.模拟延时程序.app.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<windows.h>
+void main()
+{
+printf("男：天气凉了，有个成语叫忘穿秋裤，你穿秋裤了吗？\n");
+Sleep(5000);
+printf("女:......给我圆润的走开！！！\n");
+Sleep(3000);
+printf("男：好嘞，小的这就滚\n");
+}
