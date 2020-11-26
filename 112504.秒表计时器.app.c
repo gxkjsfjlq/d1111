bad6fcd3fb10650f40c487748b7477a77472624f
gxkjsfjlq <74294932+gxkjsfjlq@users.noreply.github.com>
2020-11-26 11:44:02 +0800

    Update and rename 112503.秒表计时器.app.c to 112504.秒表计时器.app.c

---
+++ 112504.秒表计时器.app.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<windows.h>
+void main()
+{
+int x=1;
+printf("秒表计时器\n");
+do
+{
+printf("%d秒过去了\n",x);
+x++;
+Sleep(1000);
+}
+while(x<=42);
+{
+printf("303030303\n");
+}
+
+
+}
