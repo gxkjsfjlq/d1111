6af7d4aae3c82ca065fefe29e73dd819cb6a11c6
gxkjsfjlq <74294932+gxkjsfjlq@users.noreply.github.com>
2020-12-02 17:12:18 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -4,8 +4,8 @@ void main()
 int a;
 printf("请输入秒数\n");
 scanf("%d",&a);
-printf("%.2d\n",a/3600);
-printf("%.2d\n",(a%3600)/60);
+printf("%.2d:",a/3600);
+printf("%.2d:",(a%3600)/60);
 printf("%.2d\n",(a%3600)%60);
 
 
