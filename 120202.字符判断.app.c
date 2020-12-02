d37b0b3f2658795348db9de540eb4d6570924d0c
gxkjsfjlq <74294932+gxkjsfjlq@users.noreply.github.com>
2020-12-02 09:34:24 +0800

    Update 120202.字符判断.app.c

--- 120202.字符判断.app.c
+++ 120202.字符判断.app.c
@@ -1,1 +1,16 @@
 
+#include<stdio.h>
+void main()
+{
+char x;
+printf("  欢迎使用本程序\n");
+scanf("%c",&x);
+switch(x)
+{
+  case 'w':printf("  这是上键\n");break;
+  case 's':printf("  这是下键\n");break;
+  case 'a':printf("  这是左键\n");break;
+  case 'd':printf("  这是右键\n");break;
+default:printf("请输入a,s,d,w中的一个");
+}
+}
