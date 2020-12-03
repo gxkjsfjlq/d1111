b0034f65866ad10ab9aa47f99a56f840f05fdc31
gxkjsfjlq <74294932+gxkjsfjlq@users.noreply.github.com>
2020-12-03 20:58:47 +0800

    Create 120301.一维数组遍历.app.c

---
+++ 120301.一维数组遍历.app.c
@@ -0,0 +1,20 @@
+#include<stdio.h>#include<stdio.h>
+void main()
+{
+int a;
+int x[]={2,4,6,8,10};
+int y[3][4]={
+    {1,3,5,7},
+    {9,11,13,15,},
+	{17,19,21,23}
+			};
+
+for(a=0;a<5;a++)
+{
+
+	printf("%d  ",x[a]);	
+
+		}
+
+
+  }
