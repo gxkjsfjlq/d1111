4a0e6d1717a60769a4c881547132b3e149750ef3
gxkjsfjlq <74294932+gxkjsfjlq@users.noreply.github.com>
2020-12-09 17:03:30 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char x1[]="锦鲤护体";
+char x2[]="锦鲤护我";
+if(strcmp(x1,x2)==0)
+{
+printf("......\n");
+}
+else{
+	printf("......#######\n");
+		}
+
+}
