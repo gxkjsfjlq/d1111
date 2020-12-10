1c519b88405c5dd74035f3f2aeaa97e3fecc06e2
gxkjsfjlq <74294932+gxkjsfjlq@users.noreply.github.com>
2020-12-10 20:26:29 +0800

    Update 120906.吃货给我安排.app.c

--- 120906.吃货给我安排.app.c
+++ 120906.吃货给我安排.app.c
@@ -13,23 +13,23 @@ char d[]="元宵节";
 scanf("%s",&x);
 if(strcmp(x,a)==0)
 {
-printf("给我安排饺子\n");
+printf("  给我安排饺子（￣︶￣）↗　  \n");
 }
 else if(strcmp(x,b)==0)
 {
-printf("给我安排粽子\n");
+printf("  给我安排粽子[]~(￣▽￣)~*  \n");
 }
 else if(strcmp(x,c)==0)
 {
-printf("给我安排月饼\n");
+printf("  给我安排月饼φ(*￣0￣)     \n");
 }
 else if(strcmp(x,c)==0)
 {
-printf("给我安排月饼\n");
+printf("  给我安排月饼q(≧▽≦q)    \n");
 }
 else 
 {
-printf("错了错了━━(￣ー￣*|||━━重来(^人^)\n");
+printf("  错了错了━━(￣ー￣*|||━━重来(^人^)  \n");
 }
 }
 }
