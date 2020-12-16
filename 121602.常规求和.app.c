119cf3b90076647f3815148f8e854fd5e0d4214e
gxkjsfjlq <74294932+gxkjsfjlq@users.noreply.github.com>
2020-12-16 09:32:45 +0800

    Create 121602.常规求和.app.c

---
+++ 121602.常规求和.app.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int s;
+int x;
+printf("欢迎使用求和小程序\n");
+printf("请输入一个数字\n");
+scanf("%d",&x);
+if(x<=0)
+{
+printf("0");
+}
+else
+{
+printf("%d",s=(1+x)*x/2);
+}
+
+}
